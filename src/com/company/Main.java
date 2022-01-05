d5aedcfe288e04b558b85708c929314f0d7a2756
Aizhana <abd.aizhana@gmail.com>
2022-01-05 14:28:16 +0600

    my commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,25 +9,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("write  ");
+            System.out.println("write operation ");
             String input = scanner.nextLine().toUpperCase(Locale.ROOT);
-            String[] str = input.split(" ");
-            if (str.length != 3) try {
-                throw new MyException();
-            } catch (MyException e) {
-                System.out.println("Something get wrong, try again");
-                continue;
+            char[] findOperation = new char[10];
+            String operation = null;
+
+            for (int i = 0; i < input.length(); i++) {
+                findOperation[i] = input.charAt(i);
+                if (findOperation[i] == '+') {
+                    operation = "+";
+                }
+                if (findOperation[i] == '-') {
+                    operation = "-";
+                }
+                if (findOperation[i] == '*') {
+                    operation = "*";
+                }
+                if (findOperation[i] == '/') {
+                    operation = "/";
+                }
+
             }
+            String[] str = input.split("[+-/*]");
             try {
                 int numberOne = RomanNum.romanToInt(str[0]);
-                int numberTwo = RomanNum.romanToInt(str[2]);
-                String operation = str[1];
+                int numberTwo = RomanNum.romanToInt(str[1]);
                 int result = Calculate.calculate(numberOne, operation, numberTwo);
                 System.out.println(RomanNum.intToRoman(result));
             } catch (NullPointerException e) {
                 try {
                     int numberOne = Integer.parseInt(str[0]);
-                    int numberTwo = Integer.parseInt(str[2]);
+                    int numberTwo = Integer.parseInt(str[1]);
 //                    if(numberOne>10 && numberTwo>10) try {
 //                        throw new MyException();
 //                    } catch (MyException ex) {
@@ -36,7 +48,7 @@ public class Main {
                     System.out.println(Calculate.calculate(numberOne, str[1], numberTwo));
                 } catch (NumberFormatException c) {
                     System.out.println("вы вели не правильные данные! ");
-                    ;
+                    
                 }
             }
         }
